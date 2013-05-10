b2eb4499af6a40c50057964269d8b04a904248b6
asep <asep.saepudin@wgs.co.id>
2013-05-10 16:29:01 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test123::Application.config.secret_token = 'e3ee26ba83941786b7c5b1c1eb63692d7f9de4d1c533d703cdcb6eb9a67cd458dda174a504f04ae3fe11bfb3572161b9ff9c560c49dd4fd5e9aee9d72c23a894'
